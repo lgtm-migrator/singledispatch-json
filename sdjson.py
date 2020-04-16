ea7d243513f692525a6dc7f3cade62f9e6fdaf04
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-04-16 21:45:49 +0100

    Bump version: 0.1.0 → 0.2.0

--- sdjson.py
+++ sdjson.py
@@ -90,7 +90,7 @@ __author__ = "Dominic Davis-Foster"
 __copyright__ = "2020 Dominic Davis-Foster"
 
 __license__ = "LGPL3+"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __email__ = "dominic@davis-foster.co.uk"
 
 # stdlib
